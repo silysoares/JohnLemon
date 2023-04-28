1e5db47f65dc5a8c3c1c659b6cbceee38527b0e8
silysoares <104627031+silysoares@users.noreply.github.com>
2023-04-28 11:58:40 +0100

    6. Defina os valores para sua variável

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Vector3 m_Movement;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +17,8 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis ("Horizontal");
         float vertical = Input.GetAxis ("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize ();
     }
 }
