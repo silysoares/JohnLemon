2672769044f716099bfe072d2fd8d986e75f0386
silysoares <104627031+silysoares@users.noreply.github.com>
2023-05-01 18:49:12 +0100

    10. Adicionar destinos adicionais

--- Assets/Scripts/WaypointPatrol.cs
+++ Assets/Scripts/WaypointPatrol.cs
@@ -7,17 +7,20 @@ public class WaypointPatrol : MonoBehaviour
 {
     public NavMeshAgent navMeshAgent;
     public Transform[] waypoints;
-   
 
-    // Start is called before the first frame update
-    void Start()
+    int m_CurrentWaypointIndex;
+
+    void Start ()
     {
-         navMeshAgent.SetDestination(waypoints[0].position);
+        navMeshAgent.SetDestination (waypoints[0].position);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        
+        if(navMeshAgent.remainingDistance < navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination (waypoints[m_CurrentWaypointIndex].position);
+        }
     }
 }
