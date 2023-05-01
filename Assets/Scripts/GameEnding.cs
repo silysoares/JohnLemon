dca046b0f4940304b00f8e594433e55f6ca82191
silysoares <104627031+silysoares@users.noreply.github.com>
2023-05-01 17:45:17 +0100

    10. Permita que o jogador reinicie o nível

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
     public float displayImageDuration = 1f;
     public GameObject player;
-    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup exitBackgroundImageCanvasGroup; 
     public CanvasGroup caughtBackgroundImageCanvasGroup;
 
     bool m_IsPlayerAtExit;
-     bool m_IsPlayerCaught;
+    bool m_IsPlayerCaught;
     float m_Timer;
-
+    
     void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
@@ -22,27 +23,38 @@ public class GameEnding : MonoBehaviour
         }
     }
 
-   void Update ()
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    void Update ()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel (exitBackgroundImageCanvasGroup);
+            EndLevel (exitBackgroundImageCanvasGroup, false);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel (caughtBackgroundImageCanvasGroup);
+            EndLevel (caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-void EndLevel (CanvasGroup imageCanvasGroup)
+    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
-
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
-        if(m_Timer > fadeDuration + displayImageDuration)
+        if (m_Timer > fadeDuration + displayImageDuration)
         {
-            Application.Quit ();
+            if (doRestart)
+            {
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                Application.Quit ();
+            }
         }
     }
 }
