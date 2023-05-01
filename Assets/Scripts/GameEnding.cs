e509aa5fc618bb08b055b858c3215af8097e76af
silysoares <104627031+silysoares@users.noreply.github.com>
2023-05-01 15:20:52 +0100

    11. Adicione um método de atualização

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class GameEnding : MonoBehaviour
 {
-   public float fadeDuration = 1f;
-   public GameObject player;
+    public float fadeDuration = 1f;
+    public GameObject player;
 
-   void OnTriggerEnter (Collider other)
+    bool m_IsPlayerAtExit;
+
+    void OnTriggerEnter (Collider other)
     {
         if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void Update ()
+    {
+        if(m_IsPlayerAtExit)
         {
 
         }
