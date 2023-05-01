817d8070fff6aba3da215c2159988629e0328d27
silysoares <104627031+silysoares@users.noreply.github.com>
2023-05-01 17:34:29 +0100

    7. Verifique se a linha de visão do inimigo está limpa

--- Assets/Scripts/Observer.cs
+++ Assets/Scripts/Observer.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class Observer : MonoBehaviour
 {
-  public Transform player;
-  bool m_IsPlayerInRange;
+    public Transform player;
+    public GameEnding gameEnding;
 
-  void OnTriggerEnter (Collider other)
+    bool m_IsPlayerInRange;
+
+    void OnTriggerEnter (Collider other)
     {
-        if(other.transform == player)
+        if (other.transform == player)
         {
             m_IsPlayerInRange = true;
         }
     }
-    
+
     void OnTriggerExit (Collider other)
     {
-        if(other.transform == player)
+        if (other.transform == player)
         {
             m_IsPlayerInRange = false;
+        }
+    }
+
+    void Update ()
+    {
+        if (m_IsPlayerInRange)
+        {
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if (raycastHit.collider.transform == player)
+                {
+                    
+                }
+            }
         }
     }
 }
